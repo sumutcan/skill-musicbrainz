44a96c30c36d8b2e96b8d09c5f5765e1101d8fd9
Umutcan Simsek <s.umutcan@gmail.com>
2018-10-16 14:46:03 +0000

    echoing skill ready

--- __init__.py
+++ __init__.py
@@ -10,19 +10,20 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
+from rdflib import *
 
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
 
 # TODO: Change "Template" to a unique name for your skill
-class TemplateSkill(MycroftSkill):
+class MusicBrainzSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(TemplateSkill, self).__init__(name="TemplateSkill")
+        super(MusicBrainzSkill, self).__init__(name="MusicBrainzSkill")
         
         # Initialize working variables used within the skill.
-        self.count = 0
+        self.performer = "I do not know"
 
     # The "handle_xxxx_intent" function is triggered by Mycroft when the
     # skill's intent is matched.  The intent is defined by the IntentBuilder()
@@ -35,21 +36,25 @@ class TemplateSkill(MycroftSkill):
     #   'Hello world'
     #   'Howdy you great big world'
     #   'Greetings planet earth'
-    @intent_handler(IntentBuilder("").require("Hello").require("World"))
+"""     @intent_handler(IntentBuilder("").require("Hello").require("World"))
     def handle_hello_world_intent(self, message):
         # In this case, respond by simply speaking a canned response.
         # Mycroft will randomly speak one of the lines from the file
         #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("hello.world")
+        self.speak_dialog("hello.world") """
 
-    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
+    def handle who_is_singing_intent(self, message):
+        self.speak_dialog("sung.by", data={"performer": message.data["song"]})
+
+
+  """   @intent_handler(IntentBuilder("").require("Count").require("Dir"))
     def handle_count_intent(self, message):
         if message.data["Dir"] == "up":
             self.count += 1
         else:  # assume "down"
             self.count -= 1
         self.speak_dialog("count.is.now", data={"count": self.count})
-
+ """
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
     # is extremely simple, there is no need to override it.  If you DO
@@ -62,4 +67,4 @@ class TemplateSkill(MycroftSkill):
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return TemplateSkill()
+    return MusicBrainzSkill()
