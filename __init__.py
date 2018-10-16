b200495316897e36b7a6287cacb1bf0ddcb8dad4
Umutcan Simsek <s.umutcan@gmail.com>
2018-10-16 15:34:50 +0000

    fix identention and method name

--- __init__.py
+++ __init__.py
@@ -21,40 +21,15 @@ class MusicBrainzSkill(MycroftSkill):
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(MusicBrainzSkill, self).__init__(name="MusicBrainzSkill")
-        
         # Initialize working variables used within the skill.
-        self.performer = "I do not know"
+        self.performer = 0
+    
 
-    # The "handle_xxxx_intent" function is triggered by Mycroft when the
-    # skill's intent is matched.  The intent is defined by the IntentBuilder()
-    # pieces, and is triggered when the user's utterance matches the pattern
-    # defined by the keywords.  In this case, the match occurs when one word
-    # is found from each of the files:
-    #    vocab/en-us/Hello.voc
-    #    vocab/en-us/World.voc
-    # In this example that means it would match on utterances like:
-    #   'Hello world'
-    #   'Howdy you great big world'
-    #   'Greetings planet earth'
-"""     @intent_handler(IntentBuilder("").require("Hello").require("World"))
-    def handle_hello_world_intent(self, message):
-        # In this case, respond by simply speaking a canned response.
-        # Mycroft will randomly speak one of the lines from the file
-        #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("hello.world") """
-
-    def handle who_is_singing_intent(self, message):
+    @intent_handler(IntentBuilder("").require("Who").require("Sing"))
+    def handle_who_is_singing_intent(self, message):
         self.speak_dialog("sung.by", data={"performer": message.data["song"]})
 
 
-  """   @intent_handler(IntentBuilder("").require("Count").require("Dir"))
-    def handle_count_intent(self, message):
-        if message.data["Dir"] == "up":
-            self.count += 1
-        else:  # assume "down"
-            self.count -= 1
-        self.speak_dialog("count.is.now", data={"count": self.count})
- """
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
     # is extremely simple, there is no need to override it.  If you DO
